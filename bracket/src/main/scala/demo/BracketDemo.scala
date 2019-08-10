8c0c1375cfe970c60a37713e509c7a144747e2e0
Jakub Kozłowski <kubukoz@gmail.com>
2019-08-10 14:17:11 +0200

    Bracket before implementation

---
+++ bracket/src/main/scala/demo/BracketDemo.scala
@@ -0,0 +1,43 @@
+package demo
+
+import cats.effect.ExitCode
+import cats.effect.IO
+import cats.effect.IOApp
+import java.io.File
+import java.io.FileReader
+import cats.effect.Blocker
+import cats.implicits._
+import cats.effect.ContextShift
+
+object BracketDemo extends IOApp {
+  val file = new File("src/main/resources/example.txt")
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    val line: IO[String] = ???
+
+    line
+      .flatMap { line =>
+        IO { println(line) }
+      }
+      .as(ExitCode.Success)
+  }
+}
+
+object Files {
+
+  def open(file: File)(blocker: Blocker)(implicit cs: ContextShift[IO]): IO[FileReader] = blocker.delay[IO, FileReader] {
+    println("Opening file reader")
+    new FileReader(file)
+  }
+
+  def read(blocker: Blocker)(reader: FileReader)(implicit cs: ContextShift[IO]): IO[String] = blocker.delay[IO, String] {
+    val buffer = Array.fill[Char](4096)(0)
+    reader.read(buffer)
+    new String(buffer).trim
+  }
+
+  def close(blocker: Blocker)(reader: FileReader)(implicit cs: ContextShift[IO]): IO[Unit] = blocker.delay[IO, Unit] {
+    println("Closing file reader")
+    reader.close()
+  }
+}
