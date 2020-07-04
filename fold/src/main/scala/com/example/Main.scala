412f66d6719a476f89dea6344d52f546458b59ca
Jakub Kozłowski <kubukoz@gmail.com>
2020-07-05 01:40:57 +0200

    Move stack safe fold to another file

--- fold/src/main/scala/com/example/Main.scala
+++ fold/src/main/scala/com/example/Main.scala
@@ -5,22 +5,14 @@ import com.example.Status.Pending
 import com.example.Status.InProgress
 import com.example.Status.Done
 
-import cats.implicits._
-import scala.concurrent.duration.FiniteDuration
-import scala.annotation.tailrec
-import com.example.Content.Video
-import com.example.Content.Playlist
-import scala.collection.immutable.Nil
-import java.lang.StackWalker.StackFrame
-import scala.util.control.NonFatal
-import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.duration.Duration
-import cats.instances.long
-import scala.util.Random
-import cats.data.NonEmptyList
-import cats.data.Chain
-import cats.Monad
 import cats.Eval
+import cats.data.NonEmptyList
+import com.example.Content.Playlist
+import com.example.Content.Video
+import scala.concurrent.duration.FiniteDuration
+import cats.implicits._
+import scala.concurrent.duration.Duration
+import scala.util.Random
 
 // Status = Pending | In Progress (percentage: Int) | Done (at: Instant)
 
@@ -43,84 +35,6 @@ object Status {
 //Content = Video (length: FiniteDuration, link: String) | Playlist (elements: List[Content])
 sealed trait Content extends Product with Serializable {
 
-  def fold[A](video: (FiniteDuration, String) => A, playlist: NonEmptyList[A] => A): A = {
-    case class StackFrame(results: Chain[A], pendingWork: List[Content]) {
-      def addResult(result: A): StackFrame = copy(results = results.append(result))
-
-      def takeWork: Option[(Content, StackFrame)] =
-        pendingWork match {
-          case head :: tail => (head, StackFrame(results, tail)).some
-          case Nil          => None
-        }
-    }
-
-    case class Stack(frames: NonEmptyList[StackFrame]) {
-      def pop: (StackFrame, Option[Stack]) = (frames.head, frames.tail.toNel.map(Stack(_)))
-
-      def push(frame: StackFrame): Stack = Stack(frame :: frames)
-      def pushAll(another: Stack): Stack = Stack(another.frames.concatNel(frames))
-    }
-
-    object Stack {
-      def pushOrStart(stack: Option[Stack]): StackFrame => Stack =
-        frame => stack.fold(Stack(NonEmptyList.one(frame)))(_.push(frame))
-    }
-
-    //Process the current frame.
-    //This can return up to 2 frames:
-    // - a new frame, if the next unit of work is a playlist
-    // - the top frame with some potential changes (or missing, if there's no more work in it)
-    def step(topFrame: StackFrame): Either[Stack, A] =
-      topFrame.takeWork match {
-        case Some((work, topFrameRemaining)) =>
-          val appliedWork = work match {
-            case Playlist(elements) =>
-              val newFrame = StackFrame(Chain.nil, elements.toList)
-
-              //re-push the top frame, but without the current work.
-              NonEmptyList.of(newFrame, topFrameRemaining)
-
-            case Video(len, link) =>
-              val newValue = video(len, link)
-
-              NonEmptyList.one(topFrameRemaining.addResult(newValue))
-          }
-
-          Stack(appliedWork).asLeft
-
-        case None =>
-          //no more work - we can apply the results of the current frame.
-          //this is safe, because a frame must have at least one of (pending work, results).
-          val appliedFrame = playlist(topFrame.results.toList.toNel.get)
-
-          appliedFrame.asRight
-      }
-
-    //Process a single step on the stack.
-    def stepStack(stack: Stack): Either[Stack, A] = {
-      val (top, rest) = stack.pop
-
-      step(top) match {
-        case Left(frames) => rest.fold(frames)(_.pushAll(frames)).asLeft
-        case Right(value) =>
-          rest.map(_.pop) match {
-            //no more stack, no more work, done
-            case None => value.asRight
-
-            //there's more stack - we'll add the current result to the top frame
-            case Some((nextFrame, restOfRest)) => Stack.pushOrStart(restOfRest)(nextFrame.addResult(value)).asLeft
-          }
-      }
-    }
-
-    this match {
-      case Video(len, link) => video(len, link)
-      case Playlist(elements) =>
-        val initialStack = Stack(NonEmptyList.one(StackFrame(Chain.nil, elements.toList)))
-        Monad[cats.Id].tailRecM(initialStack) { stepStack }
-    }
-  }
-
   def foldEval[A](video: (FiniteDuration, String) => A, playlist: NonEmptyList[A] => A): A = {
     def go(self: Content): Eval[A] = self match {
       case Playlist(elements)  => elements.traverse(go).map(playlist)
@@ -140,13 +54,14 @@ object Content {
   final case class Video(length: FiniteDuration, link: String) extends Content
   final case class Playlist(elements: NonEmptyList[Content]) extends Content
 
-  val playlistCount: Content => Int = _.fold[Int]((_, _) => 0, 1 + _.reduce)
+  val playlistCount: Content => Int = StackSafeContentFold.fold[Int]((_, _) => 0, 1 + _.reduce)
   val playlistCountNaive: Content => Int = _.foldNaive[Int]((_, _) => 0, 1 + _.reduce)
 
-  val totalLength: Content => FiniteDuration = _.fold[FiniteDuration]((len, _) => len, _.foldLeft(Duration.Zero)(_ + _))
+  val totalLength: Content => FiniteDuration = StackSafeContentFold.fold[FiniteDuration]((len, _) => len, _.foldLeft(Duration.Zero)(_ + _))
   val totalLengthNaive: Content => FiniteDuration = _.foldNaive[FiniteDuration]((len, _) => len, _.reduceLeft(_ + _))
 
-  val links: Content => NonEmptyList[String] = _.fold[NonEmptyList[String]]((_, link) => NonEmptyList.one(link), _.flatten)
+  val links: Content => NonEmptyList[String] =
+    StackSafeContentFold.fold[NonEmptyList[String]]((_, link) => NonEmptyList.one(link), _.flatten)
   val linksNaive: Content => NonEmptyList[String] = _.foldNaive[NonEmptyList[String]]((_, link) => NonEmptyList.one(link), _.flatten)
 }
 
@@ -187,7 +102,7 @@ object Demo extends App {
   // println(arbitraryPlaylist(5, 3))
 
   List.fill(1000)(arbitraryPlaylist(5, 5)).foreach { list =>
-    val left = list.fold(Video, Playlist)
+    val left = StackSafeContentFold.fold(Video, Playlist)(list)
     val right = list
 
     val isSame = left == right
---
+++ fold/src/main/scala/com/example/StackSafeContentFold.scala
@@ -0,0 +1,89 @@
+package com.example
+
+import scala.concurrent.duration.FiniteDuration
+import cats.data.NonEmptyList
+import cats.Monad
+import cats.data.Chain
+import com.example.Content._
+import cats.implicits._
+
+object StackSafeContentFold {
+
+  def fold[A](video: (FiniteDuration, String) => A, playlist: NonEmptyList[A] => A): Content => A = {
+    case class StackFrame(results: Chain[A], pendingWork: List[Content]) {
+      def addResult(result: A): StackFrame = copy(results = results.append(result))
+
+      def takeWork: Option[(Content, StackFrame)] =
+        pendingWork match {
+          case head :: tail => (head, StackFrame(results, tail)).some
+          case Nil          => None
+        }
+    }
+
+    case class Stack(frames: NonEmptyList[StackFrame]) {
+      def pop: (StackFrame, Option[Stack]) = (frames.head, frames.tail.toNel.map(Stack(_)))
+
+      def push(frame: StackFrame): Stack = Stack(frame :: frames)
+      def pushAll(another: Stack): Stack = Stack(another.frames.concatNel(frames))
+    }
+
+    object Stack {
+      def pushOrStart(stack: Option[Stack]): StackFrame => Stack =
+        frame => stack.fold(Stack(NonEmptyList.one(frame)))(_.push(frame))
+    }
+
+    //Process the current frame.
+    //This can return up to 2 frames:
+    // - a new frame, if the next unit of work is a playlist
+    // - the top frame with some potential changes (or missing, if there's no more work in it)
+    def step(topFrame: StackFrame): Either[Stack, A] =
+      topFrame.takeWork match {
+        case Some((work, topFrameRemaining)) =>
+          val appliedWork = work match {
+            case Playlist(elements) =>
+              val newFrame = StackFrame(Chain.nil, elements.toList)
+
+              //re-push the top frame, but without the current work.
+              NonEmptyList.of(newFrame, topFrameRemaining)
+
+            case Video(len, link) =>
+              val newValue = video(len, link)
+
+              NonEmptyList.one(topFrameRemaining.addResult(newValue))
+          }
+
+          Stack(appliedWork).asLeft
+
+        case None =>
+          //no more work - we can apply the results of the current frame.
+          //this is safe, because a frame must have at least one of (pending work, results).
+          val appliedFrame = playlist(topFrame.results.toList.toNel.get)
+
+          appliedFrame.asRight
+      }
+
+    //Process a single step on the stack.
+    def stepStack(stack: Stack): Either[Stack, A] = {
+      val (top, rest) = stack.pop
+
+      step(top) match {
+        case Left(frames) => rest.fold(frames)(_.pushAll(frames)).asLeft
+        case Right(value) =>
+          rest.map(_.pop) match {
+            //no more stack, no more work, done
+            case None => value.asRight
+
+            //there's more stack - we'll add the current result to the top frame
+            case Some((nextFrame, restOfRest)) => Stack.pushOrStart(restOfRest)(nextFrame.addResult(value)).asLeft
+          }
+      }
+    }
+
+    {
+      case Video(len, link) => video(len, link)
+      case Playlist(elements) =>
+        val initialStack = Stack(NonEmptyList.one(StackFrame(Chain.nil, elements.toList)))
+        Monad[cats.Id].tailRecM(initialStack) { stepStack }
+    }
+  }
+}
