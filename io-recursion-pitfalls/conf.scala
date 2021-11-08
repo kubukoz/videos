a2a8940dbef81575b6a6701dd4b4ada7d6fe3e13
Jakub Kozłowski <kubukoz@gmail.com>
2021-11-09 00:54:18 +0100

    Move to scala-cli

---
+++ io-recursion-pitfalls/conf.scala
@@ -0,0 +1,11 @@
+// These comments are the configuration of the Scala CLI tool.
+// I haven't covered it yet, but you're encouraged to give it a try anyway!
+// https://scala-cli.virtuslab.org/
+//
+//
+//
+//using scala 2.13.7
+//using plugin org.polyvariant:::better-tostring:0.3.11
+//not using plugin com.olegpy::better-monadic-for:0.3.1
+//using lib org.typelevel::cats-effect:3.2.9
+//using lib com.kubukoz::debug-utils:1.1.3
--- io-recursion-pitfalls/project/plugins.sbt
+++
@@ -1,1 +0,0 @@
-addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.1.20")
