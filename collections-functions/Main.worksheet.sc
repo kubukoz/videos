f905c8aaab3b5e6ff238deb451255a22bb194bb9
Jakub Kozłowski <kubukoz@gmail.com>
2022-02-12 03:11:57 +0100

    collection functions wip

---
+++ collections-functions/Main.worksheet.sc
@@ -0,0 +1,8 @@
+val aList = List(1, 5, 10)
+
+val bSet = Set("cow", "dog", "cat")
+
+val cMap = Map(
+  10 -> List(1, 2, 3),
+  20 -> List(40),
+)
---
+++ collections-functions/conf.scala
@@ -0,0 +1,9 @@
+// These comments are the configuration of the Scala CLI tool.
+// I haven't covered it yet, but you're encouraged to give it a try anyway!
+// https://scala-cli.virtuslab.org/
+//
+//
+//
+//using scala 2.13.7
+//using plugin org.polyvariant:::better-tostring:0.3.11
+//using lib com.kubukoz::debug-utils:1.1.3
