b60dd841b11310f2248004d2d6c210a962f9e7e6
Jakub Kozłowski <kubukoz@gmail.com>
2022-11-09 14:10:32 +0100

    restore init

--- faround2/main.scala
+++ faround2/main.scala
@@ -1,26 +1,3 @@
 object main extends App {
-
-  object <<<<<<< {
-    def HEAD(a: Int) = ???
-  }
-
-  implicit class StringOpsGit(s: String) {
-    def =======(s2: String) = ???
-    def >>>>>>>(s2: String) = ???
-  }
-
-  val git1 = "foo"
-
-  println(
-<<<<<<< HEAD
-"hello!"
-=======
-"world"
->>>>>>> git1
-  )
-
-  // desugared
-  println(
-    <<<<<<<.HEAD("hello!".=======("world".>>>>>>>(git1)))
-  )
+println("hello")
 }
