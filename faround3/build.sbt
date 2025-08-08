24c4c43981e073df9d5599f98961120e23e24ec3
Jakub Kozłowski <kubukoz@gmail.com>
2025-08-08 18:31:17 +0200

    Finish video

--- faround3/build.sbt
+++ faround3/build.sbt
@@ -1,9 +1,9 @@
 val compilerPlugins = List(
-  compilerPlugin("com.kubukoz" %% "better-tostring" % "0.3.5" cross CrossVersion.full)
+  compilerPlugin("org.polyvariant" % "better-tostring" % "0.3.17" cross CrossVersion.full)
 )
 
 val commonSettings = Seq(
-  scalaVersion := "3.0.1",
+  scalaVersion := "3.7.2",
   fork in Test := true,
   libraryDependencies ++= compilerPlugins,
   scalacOptions -= "-Xfatal-warnings",
--- faround3/src/main/scala/demo/Main.scala
+++ faround3/src/main/scala/demo/Main.scala
@@ -1,12 +1,38 @@
-final case class Person(name: String)
+import language.experimental.modularity
+
+final case class Person(tracked val name: String)
 
 object Main {
 
   def f(p: Person): p.name.type =
     p.name
 
-  val p: Person = Person("John")
+  final val name = "John"
 
-  val x: p.name.type = f(p)
+  final val p = Person(name)
 
+  val x: "John" = f(p)
+
+  val jk = p.copy(name = "Jakub")
+
+}
+
+case class Keys(tracked val keys: List[String], extra: String)
+
+def validate(keys: Keys): Either[Throwable, Keys { val keys: ::[String]}] = {
+  keys.keys match {
+    case Nil => Left(new IllegalArgumentException("Keys cannot be empty"))
+    case head :: next => Right(keys.copy(keys = ::(head, next)))
+  }
+}
+
+@main def run = {
+  val initKeysForValidation = Keys(List("a", "b"), extra = "c")
+
+  validate(initKeysForValidation).foreach { validatedKeys =>
+
+    validatedKeys.keys match {
+      case head :: next => println(s"definitely not empy! $head, $next")
+    }
+  }
 }
